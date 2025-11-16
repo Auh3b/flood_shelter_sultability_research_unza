de70b7be24548711889d7f41558730409bf82f52
auh3b <ngaiyayec@yahoo.com>
2025-11-16 17:50:14 +0200

    raster clip and calc

--- process_engine/raster.py
+++ process_engine/raster.py
@@ -1,4 +1,5 @@
-import rasterio
+from osgeo import gdal
+import numpy as np
 
 
 def raster_calculate(expression):
@@ -11,3 +12,13 @@ def raster_to_vector(raster):
 
 def reclassify_by_value(raster):
     return
+
+def uniqueValuesReport(path:str, name:str):
+    output = None
+    with gdal.Open(path) as ds:
+        arr  = ds.GetRasterBand(1).GetBandAsArray()
+        values, counts = np.unique(arr, return_counts=True)
+        output = {x: y for x, y in zip(values, counts)}
+    
+    output['name'] = name
+    return output
